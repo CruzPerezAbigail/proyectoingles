ed71293b3b6faeb63aba98d5b172bea24524d7b8
CruzPerezAbigail <abigail07cruz@gmail.com>
2023-03-10 16:27:19 -0600

    10032023

---
+++ app/models/user.rb
@@ -0,0 +1,26 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+         def administrador?
+          self.role == 'administrador'    
+        end
+        
+        def ingles?
+          self.role == 'ingles'
+        end
+        def financieros?
+          self.role == 'financieros'
+        end
+
+        def servicios?
+          self.role == 'servicios'
+        end
+        def direccion?
+          self.role == 'direccion'
+        end
+      
+      
+end
